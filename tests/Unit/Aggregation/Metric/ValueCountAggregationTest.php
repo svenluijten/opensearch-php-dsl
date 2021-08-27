0e1196272069780983ed17280c336f2b97d688c3
Lennart Tinkloh <lennart@tinkloh.eu>
2021-08-27 21:18:21 +0200

    fix: cs fixes

--- tests/Unit/Aggregation/Metric/ValueCountAggregationTest.php
+++ tests/Unit/Aggregation/Metric/ValueCountAggregationTest.php
@@ -2,8 +2,6 @@
 
 namespace ONGR\ElasticsearchDSL\Tests\Unit\Aggregation\Metric;
 
-use ONGR\ElasticsearchDSL\Aggregation\Metric\MaxAggregation;
-use ONGR\ElasticsearchDSL\Aggregation\Metric\SumAggregation;
 use ONGR\ElasticsearchDSL\Aggregation\Metric\ValueCountAggregation;
 use PHPUnit\Framework\TestCase;
 
